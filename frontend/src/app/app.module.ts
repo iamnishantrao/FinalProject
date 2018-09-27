a5004b9ccf0880330a20411815b3380e0c34a03e
iamprakhar016 <prakharg016@gmail.com>
2018-09-27 15:51:45 +0530

    footer change

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatCardModule,MatIconModule, MatListModule} from '@angular/material';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { NgMatSearchBarModule } from 'ng-mat-search-bar';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,NgMatSearchBarModule,MatCardModule,
+    BrowserAnimationsModule,MatToolbarModule, LayoutModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
